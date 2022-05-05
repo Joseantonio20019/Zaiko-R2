27544be3c6337dfbda397fed880227a72425fe33
Jose <joseantonio20019@gmail.com>
2022-05-05 14:34:26 +0100

    Rutas principales hechas

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\MarkController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\UbicationController;
+use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\StatusController;
+use App\Http\Controllers\ModelController;
+use App\Http\Controllers\RegisterController;
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -89,6 +94,80 @@ Route::put('/marks/edit/{id}',[MarkController::class,'update']);
 Route::delete('/marks/delete/{id}',[MarkController::class,'destroy']);
 
 
+//Status Routes
+
+
+Route::get('/status', [StatusController::class, 'index']);
+
+Route::get('/status/create', [StatusController::class, 'create']);
+
+Route::post('/status/create',[StatusController::class,'store']);
+
+Route::get('/status/edit/{id}',[StatusController::class,'edit']);
+
+Route::put('/status/edit/{id}',[StatusController::class,'update']);
+
+Route::delete('/status/delete/{id}',[StatusController::class,'destroy']);
+
+
+//Family Routes
+
+Route::get('/family', [FamilyController::class, 'index']);
+
+Route::get('/family/create', [FamilyController::class, 'create']);
+
+Route::post('/family/create',[FamilyController::class,'store']);
+
+Route::get('/family/edit/{id}',[FamilyController::class,'edit']);
+
+Route::put('/family/edit/{id}',[FamilyController::class,'update']);
+
+Route::delete('/family/delete/{id}',[FamilyController::class,'destroy']);
+
+
+//Model Routes
+
+Route::get('/model', [ModelController::class, 'index']);
+
+Route::get('/model/create', [ModelController::class, 'create']);
+
+Route::post('/model/create',[ModelController::class,'store']);
+
+Route::get('/model/edit/{id}',[ModelController::class,'edit']);
+
+Route::put('/model/edit/{id}',[ModelController::class,'update']);
+
+Route::delete('/model/delete/{id}',[ModelController::class,'destroy']);
+
+
+//Register Routes
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::get('/register/create', [RegisterController::class, 'create']);
+
+Route::post('/register/create',[RegisterController::class,'store']);
+
+Route::get('/register/edit/{id}',[RegisterController::class,'edit']);
+
+Route::put('/register/edit/{id}',[RegisterController::class,'update']);
+
+Route::delete('/register/delete/{id}',[RegisterController::class,'destroy']);
+
+
+//Devices Routes
+
+Route::get('/devices', [DeviceController::class, 'index']);
+
+Route::get('/devices/create', [DeviceController::class, 'create']);
+
+Route::post('/devices/create',[DeviceController::class,'store']);
+
+Route::get('/devices/edit/{id}',[DeviceController::class,'edit']);
+
+Route::put('/devices/edit/{id}',[DeviceController::class,'update']);
+
+Route::delete('/devices/delete/{id}',[DeviceController::class,'destroy']);
 
 
 
