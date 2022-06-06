e2fef398daee0bbad6d0f5251e5a2ac0a8efea1c
Denis <denis.perdomo.p@gmail.com>
2022-06-06 15:03:33 +0100

    Update Create.vue
    Linea 47. Cambio en v-model

--- resources/js/Pages/Devices/Computers/Create.vue
+++ resources/js/Pages/Devices/Computers/Create.vue
@@ -14,7 +14,7 @@
                     Site
                 </label>
                 
-                    <select v-model="selectedsite" @change="getUbications()" class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+                    <select v-model="form.selectedsite" @change="getUbications()" class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
                         <option v-for="site in sites" :value="site.id" :key="site.id">{{site.name}}</option>
                     </select>
                 
