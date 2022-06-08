f0f9154f8af336561ed9e342029157372aec3550
Denis <denis.perdomo.p@gmail.com>
2022-06-08 08:06:51 +0100

    Update MonitorController.php
    Cambios en:
    Línea 136. Añadir selectedsite a la validación
    Línea 142. Cambiar por el valor correcto de $data

--- app/Http/Controllers/MonitorController.php
+++ app/Http/Controllers/MonitorController.php
@@ -133,12 +133,13 @@ class MonitorController extends Controller
             'user' => ['required'],
             'register_comment' => ['nullable'],
             'ubication' => ['required'],
+            'selectedsite' => ['required'],
             'department' => ['required'],
             'modification_date' => ['nullable'],
         ]);
 
         $ubication = Ubication::where('id',$data['ubication'])->first();
-        $site =  Site::where('id',$data['ubication'])->first();
+        $site =  Site::where('id',$data['selectedsite'])->first();
         $department = Department::where('id',$data['department'])->first();
 
         $device = Device::create([
